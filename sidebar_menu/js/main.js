01352cf46cf27656e9873f8f2d72cb352fc9a88a
Haokun Wang <terryneverstop@gmail.com>
2017-08-04 17:11:48 -0700

    8/4/2017 Update

---
+++ sidebar_menu/js/main.js
@@ -0,0 +1,19 @@
+// Write your pseudo code here! HAPPY CODING! :)
+
+//Hide the side bar in CSS (.sidebar)
+
+// When the user clicks on .hamburger
+  // Add the active class to the .sidebar (uses translateX to slide in)
+
+$('.hamburger').on('click', function() {
+  $('.sidebar').addClass('active');
+  $('.content').addClass('slide-right');
+});
+
+// When the user clicks on .close
+  // Remove the active class to the .sidebar (uses translateX to slide in)
+
+$('.close').on('click', function() {
+  $('.sidebar').removeClass('active');
+  $('.content').removeClass('slide-right');
+});
